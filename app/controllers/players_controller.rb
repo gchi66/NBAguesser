a257b4b235ce3c9289f6049e4838913193e744e6
grant chichester <gchichester@gmail.com>
2023-06-29 09:47:16 +0700

    nothing works, just pushing to save progress

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,11 +2,15 @@ class PlayersController < ApplicationController
   def index
 
   end
+
   def player_stats
     season = params[:season] # Retrieve the selected season from the params
-
     # Fetch player statistics for the selected season using the get_player_stats method
     @player_stats = get_player_stats(season)
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @player_stats }
+    end
   end
 
   private
@@ -16,20 +20,20 @@ class PlayersController < ApplicationController
     # ...
   end
 
-  def show
-    # Fetch player details using player ID
-    @player = NBAApi.get_player(params[:id])
-  end
+  # def show
+  #   # Fetch player details using player ID
+  #   @player = NBAApi.get_player(params[:id])
+  # end
 
-  def stats
-    # Fetch player statistics using player ID
-    @player_stats = NBAApi.get_player_stats(params[:id])
-  end
+  # def stats
+  #   # Fetch player statistics using player ID
+  #   @player_stats = NBAApi.get_player_stats(params[:id])
+  # end
 
-  def fg_percentage
-    # Fetch player field goal percentage using player ID
-    @fg_percentage = NBAApi.get_player_fg_percentage(params[:id])
-  end
+  # def fg_percentage
+  #   # Fetch player field goal percentage using player ID
+  #   @fg_percentage = NBAApi.get_player_fg_percentage(params[:id])
+  # end
 
 
 end
--- lib/nba_api.rb
+++ lib/nba_api.rb
@@ -3,39 +3,6 @@ require 'net/http'
 require 'openssl'
 require 'json'
 
-def get_seasons
-  url = URI("https://api-nba-v1.p.rapidapi.com/seasons")
-  api_key = ENV.fetch('NBA_API_KEY')
-
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  request = Net::HTTP::Get.new(url)
-  request["X-RapidAPI-Key"] = api_key
-  request["X-RapidAPI-Host"] = 'api-nba-v1.p.rapidapi.com'
-
-  response = http.request(request)
-  data = JSON.parse(response.read_body)
-  return data
-
-def get_teams
-  url = URI("https://api-nba-v1.p.rapidapi.com/teams")
-  api_key = ENV.fetch('NBA_API_KEY')
-
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-  request = Net::HTTP::Get.new(url)
-  request["X-RapidAPI-Key"] = api_key
-  request["X-RapidAPI-Host"] = 'api-nba-v1.p.rapidapi.com'
-
-  response = http.request(request)
-  data = JSON.parse(response.read_body)
-  return data
-end
-
 def get_player_stats(season)
   url = URI("https://api-nba-v1.p.rapidapi.com/players?#{season}")
   api_key = ENV.fetch('NBA_API_KEY')
@@ -49,6 +16,18 @@ def get_player_stats(season)
   request["X-RapidAPI-Host"] = 'api-nba-v1.p.rapidapi.com'
 
   response = http.request(request)
+  # Check the response code
+  puts "Response Code: #{response.code}"
+
+  # Check the response headers
+  puts "Response Headers:"
+  response.each_header do |key, value|
+    puts "#{key}: #{value}"
+  end
+
+  # Check the response body
+  puts "Response Body:"
+  puts response.body
   data = JSON.parse(response.read_body)
   processed_data = data['api']['statistics'].map do |player|
     {
@@ -63,7 +42,4 @@ def get_player_stats(season)
   end
 
   return processed_data
-end
-
-
 end
