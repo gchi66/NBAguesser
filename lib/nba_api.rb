cc7107b2c36ebb803537606012ef7cc21ff9acd3
grant chichester <gchichester@gmail.com>
2023-07-06 17:22:13 +0700

    data is gettable but api doesn't provide correct data. switch apis

--- lib/nba_api.rb
+++ lib/nba_api.rb
@@ -49,23 +49,29 @@ class NbaApi
     end
 
     # Check the response body
-    puts "Response Body:"
-    puts response.body
     data = JSON.parse(response.read_body)
-    processed_data = data['response'].map do |player_data|
-      team = player_data['team']
-      player = player_data['player']
-    {
-      name: "#{player['firstname']} #{player['lastname']}",
-      team: team['name'],
-      image_url: team['logo'],
-      points: player_data['points'],
-      rebounds: player_data['totReb'],
-      assists: player_data['assists']
-      # Add other necessary player stats
-    }
-    end
+    formatted_response = JSON.pretty_generate(data)
+    puts "Formatted Response Body:"
+    puts formatted_response
 
+    processed_data = []
+
+    if data['response'].present?
+      processed_data = data['response'].map do |player_data|
+        team = player_data['team']
+        player = player_data['player']
+        {
+          name: "#{player['firstname']} #{player['lastname']}",
+          team: team ? team['name'] : 'Unknown',
+          image_url: team ? team['logo'] : '',
+          points: player_data['points'] || 0,
+          rebounds: player_data['totReb'] || 0,
+          assists: player_data['assists'] || 0
+          # Add other necessary player stats
+        }
+      end
+    end
+    puts processed_data[0]
     return processed_data[0]
 
   end
