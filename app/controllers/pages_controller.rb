2964681e9657e2a7d2d951b29c8a575a6c08eace
grant chichester <gchichester@gmail.com>
2023-07-19 11:35:24 +0700

    trouble getting the player images, but everything else works.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
-require 'nba_api'
 require 'nokogiri'
 require 'open-uri'
+require 'net/http'
+
 class PagesController < ApplicationController
 
   def home
+    get_correct_player
+    get_guess_players
+    render 'home'
+  end
+
+  def get_correct_player
     if params[:season].present? && params[:season][:season].present?
       season = params[:season][:season]
 
@@ -19,28 +26,76 @@ class PagesController < ApplicationController
 
       player_row = selected_rows.sample
 
-      puts "HERE IS THE FUCKING PLAYER ROW YOU CUNTBAG:"
+      puts "Here is the player row:"
       puts player_row
 
       if player_row
-        @random_player = parse_player_data(player_row)
+        @correct_player = parse_player_data(player_row)
+        @correct_player[:image_url] = fetch_player_image(@correct_player[:player_id])
       else
-        @random_player = {}
+        @correct_player = {}
       end
     end
-    render 'home'
+  end
+
+  def get_guess_players
+    if params[:season].present? && params[:season][:season].present?
+      season = params[:season][:season]
+
+      url = "https://www.basketball-reference.com/leagues/NBA_#{season}_per_game.html"
+
+      doc = Nokogiri::HTML(URI.open(url))
+
+      players_row = doc.css('tr.full_table')
+
+      selected_rows = players_row[1..380].to_a
+
+      # player_row = selected_rows.sample
+
+      # puts "Here is the player row:"
+      # puts player_row
+
+      @guess_players = selected_rows.sample(3).map do |player_row|
+        player_info = parse_player_data(player_row)
+        player_info[:image_url] = fetch_player_image(player_info[:player_id])
+        player_info
+      end
+    else
+      @guess_players = []
+    end
   end
 
   def parse_player_data(player_row)
+    return {} unless player_row
 
+    player_link = player_row.at_css('td[data-stat="player"] a')
+    profile_url = "https://www.basketball-reference.com#{player_link['href']}"
+    player_id = player_link['href'].split('/').last.split('.').first
 
     {
-      name: player_row.at('td[data-stat="player"]').text,
-      team: player_row.at('td[data-stat="team_id"] a').text,
-      points: player_row.at('td[data-stat="pts_per_g"]').text,
-      rebounds: player_row.at('td[data-stat="trb_per_g"]').text,
-      assists: player_row.at('td[data-stat="ast_per_g"]').text
+      name: player_link.text,
+      team: player_row.at_css('td[data-stat="team_id"] a')&.text || "N/A",
+      points: player_row.at_css('td[data-stat="pts_per_g"]').text,
+      rebounds: player_row.at_css('td[data-stat="trb_per_g"]').text,
+      assists: player_row.at_css('td[data-stat="ast_per_g"]').text,
+      profile_url: profile_url,
+      image_url: fetch_player_image(profile_url), # Fetch the player's image URL
+      player_id: player_id
     }
+  end
+
+  def fetch_player_image(player_url)
+    return nil unless player_url
+
+    img_tag = nil
+    begin
+      doc = Nokogiri::HTML(URI.open(player_url))
+      img_tag = doc.at_css('img[itemprop="image"]')
+    rescue OpenURI::HTTPError => e
+      puts "Error fetching player image: #{e.message}"
+    end
+
+    img_tag['src'] if img_tag
   end
 
   # private
