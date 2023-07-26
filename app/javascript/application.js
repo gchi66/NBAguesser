afb11e42bef8656c6153a998bb0e86f0df231938
grant chichester <gchichester@gmail.com>
2023-07-26 14:22:41 +0700

    basic css and figured out webpack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+document.addEventListener("DOMContentLoaded", function(){
+  const playerCards = document.querySelectorAll(".player-card");
+
+  playerCards.forEach(card => {
+    card.addEventListener("click", function(){
+      console.log("hello");
+    });
+  });
+});
