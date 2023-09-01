efc66dbb97bd87d511ec441a53993145a89b166e
grant chichester <gchichester@gmail.com>
2023-09-02 03:03:42 +0700

    localStorage logic completed. dailystreaks etc etc all functional

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,35 +14,35 @@ document.addEventListener("DOMContentLoaded", function(){
   // const csrfToken = document.querySelector("meta[name='csrf-token']").content;
   // console.log(csrfToken);
 
-    // 6 AM LOGIC VVVVVVVV
-    function scheduleResetAt6AM() {
-      // Calculate the time until the next 6 am
+    // MIDNIGHT LOGIC VVVVVVVV
+    function scheduleResetAtMidnight() {
+      // Calculate the time until the next midnight
       const now = new Date();
-      const sixAM = new Date(now);
-      sixAM.setHours(6, 0, 0, 0);
+      const midnight = new Date(now);
+      midnight.setHours(24, 0, 0, 0);
 
-      // If it's already past 6 am, schedule it for tomorrow
-      if (now > sixAM) {
-        sixAM.setDate(now.getDate() + 1);
+      // If it's already past midnight schedule it for tomorrow
+      if (now > midnight) {
+        midnight.setDate(now.getDate() + 1);
       }
 
-      const timeUntil6AM = sixAM - now;
+      const timeUntilMidnight = midnight - now;
 
-      // reset the guesses at 6am
+      // reset the guesses at midnight
       setTimeout(function () {
         // Reset total guesses and any other relevant counters
-        localStorage.setItem("total_guesses", 0);
 
+        localStorage.setItem("total_daily_guesses", 0);
         // Call any other functions or logic needed for the reset
         // ...
 
         // Schedule the next reset for the following day
-        scheduleResetAt6AM();
-      }, timeUntil6AM);
+        scheduleResetAtMidnight();
+      }, timeUntilMidnight);
     }
 
     // Initial call to schedule the first reset
-    scheduleResetAt6AM();
+    scheduleResetAtMidnight();
 
 
   // GENERATING UNIQUE DEVICE ID BASED ON TIMESTAMP
@@ -64,6 +64,8 @@ document.addEventListener("DOMContentLoaded", function(){
 
   document.addEventListener("turbo:load", function() {
     console.log('turbo loaded');
+
+
     // MODAL LOGIC VVVV
 
     var modal = document.getElementById("myModal");
@@ -72,6 +74,10 @@ document.addEventListener("DOMContentLoaded", function(){
     const statsContainer = document.querySelector(".stats-container");
     const instructionsContainer = document.querySelector(".instructions-container");
     const dailyChallengesCompleted = parseInt(localStorage.getItem("daily_challenges_completed")) || 0;
+    // initializing the total daily guesses variable
+    const totalDailyGuesses = parseInt(localStorage.getItem("total_daily_guesses")) || 0;
+    // initializing the total guesses variable
+    const totalGuesses = parseInt(localStorage.getItem("total_guesses")) || 0;
     // var pageContainer = document.querySelector(".page-container");
 
 
@@ -80,14 +86,14 @@ document.addEventListener("DOMContentLoaded", function(){
       modal.style.display = "block";
       statsContainer.classList.remove("hide-element");
       instructionsContainer.classList.add("hide-element");
-      // star emoji for each time they've completed a challenge
-      const starEmoji = "⭐".repeat(dailyChallengesCompleted);
       // initializing daily streak
       const dailyStreak = parseInt(localStorage.getItem("daily_streak")) || 0;
       const streakStartDate = localStorage.getItem("streak_start_date");
       const today = new Date().toISOString().split("T")[0];
       // figuring out the current streak
       const consecutiveDays = streakStartDate === today ? dailyStreak : 0;
+      // star emoji for each time they've completed a challenge
+      const starEmojis = "⭐".repeat(consecutiveDays);
       // calculating win percentage
       const correctGuesses = parseInt(localStorage.getItem("correct_guesses")) || 0;
       const totalGuesses = parseInt(localStorage.getItem("total_guesses")) || 0;
@@ -98,24 +104,23 @@ document.addEventListener("DOMContentLoaded", function(){
       const statsContent1 = document.getElementById("statsContent1");
       const statsContent2 = document.getElementById("statsContent2");
 
-      statsContent.innerHTML = `Daily Challenges Completed: ${starEmoji}<br>`;
-      statsContent1.innerHTML = `Current Streak: ${consecutiveDays}<br>`;
-      statsContent2.innerHTML = `Win Percentage: ${winPercentage.toFixed(2)}%`;
+      statsContent.innerHTML = `Current Streak: (${consecutiveDays})${starEmojis}<br>`
+      statsContent1.innerHTML = `Win Percentage: ${winPercentage.toFixed(2)}%`;
+      statsContent2.innerHTML =`Total challenges completed: ${dailyChallengesCompleted}`
     }
 
     btnInstructions.onclick = function() {
       modal.style.display = "block";
       statsContainer.classList.add("hide-element");
       instructionsContainer.classList.remove("hide-element");
-      const starEmoji = "⭐"
 
       const instructionsContent = document.getElementById("instructionsContent");
       const instructionsContent1 = document.getElementById("instructionsContent1");
       const instructionsContent2 = document.getElementById("instructionsContent2");
 
-      instructionsContent.innerHTML = "Select a season, then guess the NBA player based on the stats provided.<br>";
-      instructionsContent1.innerHTML = `Each day you can try to guess 5 players. Get 3/5 correct and you'll have completed the daily challenge and earned yourself a ${starEmoji}!<br>`;
-      instructionsContent2.innerHTML = `Complete the daily challenge and get a ${starEmoji} everyday to keep your daily streak going!<br>`;
+      instructionsContent.innerHTML = "Select a season, then guess the NBA player based on the stats provided.";
+      instructionsContent1.innerHTML = `Guess 3 players correct out of your 5 daily guesses to earn yourself a ⭐!`;
+      instructionsContent2.innerHTML = `How long can you keep your daily streak going for? 🤔`;
     }
 
     var closeBtn = document.getElementsByClassName("close")[0];
@@ -136,6 +141,13 @@ document.addEventListener("DOMContentLoaded", function(){
         event.preventDefault();
         console.log("Form submitted via AJAX");
 
+        // limiting the totalDailyGuesses to 5
+        const messageContainer = document.getElementById("message-container");
+        if (totalDailyGuesses  >= 5) {
+          messageContainer.innerHTML = `<h4>Out of guesses, try again tomorrow<h4>`;
+          return;
+        }
+
         const formData = new FormData(form);
 
         console.log(formData);
@@ -148,28 +160,15 @@ document.addEventListener("DOMContentLoaded", function(){
             console.log("ajax successfully fired");
 
 
-            // initializing the total guesses variable
-            const totalGuesses = parseInt(localStorage.getItem("total_guesses")) || 0;
-
-            // Checking if the user has exceeded the daily limit of 5 guesses
-            // if (totalGuesses >= 5) {
-            //   // TODO IMPLEMENT LOGIC FOR GOING TO THE "EXCEEDED DAILY GUESSES PAGE"
-            //   // window.location.href = LimitPageURL;
-            //   return; // Exit the function, no further processing
-            // }
             // SEASON SELECTION LOGIC AND APPEARING HEADER LOGIC
 
             const headingContainer = document.querySelector(".heading-container");
-            const messageContainer = document.getElementById("message-container");
             const welcomeHeader = document.getElementById("welcome-header");
             const formContainer = document.querySelector(".form-container");
             const playerCardContainer = data.querySelector(".player-card-container");
             const correctPlayerCard = data.querySelector(".correct-player-card");
 
-            if (totalGuesses  >= 5) {
-              messageContainer.innerHTML = "Out of guesses, try again tomorrow"
-            }
-            else if (formData.get("season[season]")) {
+            if (formData.get("season[season]")) {
               messageContainer.innerHTML = `<h4>Selected season: ${formData.get("season[season]")}</h4>`;
               seasonForm.classList.add("hide-element");
               welcomeHeader.classList.add("hide-element");
@@ -202,7 +201,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
                 const currentDate = new Date().toISOString().split("T")[0];
 
-                // incrementing total guesses
+                // incrementing total daily guesses
+                localStorage.setItem("total_daily_guesses", totalDailyGuesses + 1);
+
+                // incrementing total guesses for all time (to caluculate winning %)
                 localStorage.setItem("total_guesses", totalGuesses + 1);
 
                 const playerName = card.dataset.playerName;
@@ -216,14 +218,17 @@ document.addEventListener("DOMContentLoaded", function(){
                   if (userGuessCorrect) {
                     const currentStreak = parseInt(localStorage.getItem("daily_streak")) || 0;
                     const correctGuesses = parseInt(localStorage.getItem("correct_guesses")) || 0;
+                    const streakStartDate = localStorage.getItem("streak_start_date");
 
                     // user must get 3/5 guesses correct
                     if (correctGuesses % 5 === 2) {
                       localStorage.setItem("daily_streak", currentStreak + 1);
-                      localStorage.setItem("streak_start_date", currentDate);
+                      // incrementing daily challenges
+                      localStorage.setItem("daily_challenges_completed", dailyChallengesCompleted + 1);
+                      if (!streakStartDate) {
+                        localStorage.setItem("streak_start_date", currentDate);
+                      }
                     }
-                    // incrementing daily challenges
-                    localStorage.setItem("daily_challenges_completed", dailyChallengesCompleted + 1);
 
                     // incrementing correct guesses count
                     localStorage.setItem("correct_guesses", correctGuesses + 1);
