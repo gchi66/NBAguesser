399fefb5cbb2cbb69649dfbbc2eafbffc7ef10db
grant chichester <gchichester@gmail.com>
2023-09-30 22:27:42 +0700

    changed app.rb to only use the dotenv gem during development

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require 'dotenv/rails-now' if Rails.env.development? || Rails.env.test?
 
 module NBAguesser
   class Application < Rails::Application
